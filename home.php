a9523f3c9b42d8205f33836810f52beaae243764
Keito Teranishi <teranishi@undapt.jp>
2015-07-01 00:29:34 +0900

    ヘッダーとフッターの読み込み

--- home.php
+++ home.php
@@ -7,5 +7,7 @@
     <link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/home.css">
   </head>
   <body class="home">
+    <?php get_template_part('layouts/header') ?>
+    <?php get_template_part('layouts/footer') ?>
   </body>
 </html>
