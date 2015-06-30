1b902534558cc7b76aaa5589bee88a9a929e293c
Keito Teranishi <teranishi@undapt.jp>
2015-07-01 01:46:55 +0900

    タイトルの記述、スタイルの読み込み、ファイル名のクラスの記述

--- category.php
+++ category.php
@@ -2,7 +2,11 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/home.css">
+    <link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/category.css">
   </head>
-  <body>
+  <body class="category">
   </body>
 </html>
