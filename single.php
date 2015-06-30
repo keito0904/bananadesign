4e327a73bd637095d2799109ae93b4acb1c29138
Keito Teranishi <teranishi@undapt.jp>
2015-07-01 01:30:26 +0900

    sub-headerの記述

--- single.php
+++ single.php
@@ -9,6 +9,11 @@
   </head>
   <body class="single">
     <?php get_template_part('layouts/header') ?>
+    <div class="sub-header">
+      <div class="content">
+        <h1>ブログ</h1>
+      </div>
+    </div>
     <?php get_template_part('layouts/footer') ?>
   </body>
 </html>
