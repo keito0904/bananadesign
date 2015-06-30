27c2503fedbfe5cf6ac98f7d7b6777d5f5fc6470
Keito Teranishi <teranishi@undapt.jp>
2015-06-30 16:14:20 +0900

    ナビゲーションの記述

--- about.php
+++ about.php
@@ -12,5 +12,30 @@ Template Name:about
     <link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/about.css">
   </head>
   <body class="about">
+    <nav class="top-nav">
+      <ul class="content">
+        <li class="logo">
+          <a href="<?php bloginfo('url'); ?>" class="nav-logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png"></a>
+        </li>
+        <li class="list-border"></li>
+        <li>
+          <a href="<?php bloginfo('url'); ?>" class="nav-link <?php if ( is_page('top') ) { echo 'active'; } ?>">ホーム</a>
+        </li>
+        <li class="list-border"></li>
+        <li>
+          <a href="<?php bloginfo('url'); ?>/about" class="nav-link <?php if ( is_page('about') ) { echo 'active'; } ?>">組織概要</a>
+        </li>
+        <li class="list-border"></li>
+        <li>
+          <a href="<?php bloginfo('url'); ?>/service" class="nav-link <?php if ( is_page('service') ) { echo 'active'; } ?>">事業内容</a>
+        </li>
+        <li class="list-border"></li>
+        <li>
+          <a href="<?php bloginfo('url'); ?>/blog" class="nav-link <?php if ( is_page('blog') ) { echo 'active'; } ?>">ブログ</a>
+        </li>
+        <li class="list-border"></li>
+        <div class="clear"></div>
+      </ul>
+    </nav>
   </body>
 </html>
