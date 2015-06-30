54d38377e602e42d94965a4dd054d7c85b71f50b
Keito Teranishi <teranishi@undapt.jp>
2015-07-01 00:41:01 +0900

    sub-headerの記述

--- home.php
+++ home.php
@@ -8,6 +8,11 @@
   </head>
   <body class="home">
     <?php get_template_part('layouts/header') ?>
+    <div class="sub-header">
+      <div class="content">
+        <h1>ブログ</h1>
+      </div>
+    </div>
     <?php get_template_part('layouts/footer') ?>
   </body>
 </html>
