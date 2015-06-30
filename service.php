53ea69396b275ebc4ce05d2cb5cd6dc7d733b1f3
Keito Teranishi <teranishi@undapt.jp>
2015-06-30 21:48:54 +0900

    タイトルの記述、スタイルの読み込み、ファイル名のクラスの記述

--- service.php
+++ service.php
@@ -7,7 +7,10 @@ Template Name:service
 <html>
   <head>
     <meta charset="UTF-8">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/service.css">
   </head>
-  <body>
+  <body class="service">
   </body>
 </html>
