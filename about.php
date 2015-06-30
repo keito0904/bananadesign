94fbc6ba805f38214f390d2d359f5b08c834538d
Keito Teranishi <teranishi@undapt.jp>
2015-06-30 21:43:35 +0900

    フッターのテンプレート化

--- about.php
+++ about.php
@@ -103,27 +103,6 @@ Template Name:about
         <div class="clear"></div>
       </div>
     </div>
-    <div class="footer">
-      <div class="content">
-        <div class="footer-info">
-          <strong>会社情報</strong>
-          <div class="info">株式会社バナナデザイン</div>
-          <div class="clear"></div>
-        </div>
-        <div class="footer-info">
-          <strong>所在地</strong>
-          <div class="info">東京都渋谷区神泉町8-16 渋谷ファーストプレイス</div>
-          <div class="clear"></div>
-        </div>
-        <div class="footer-info">
-          <strong>E-Mail</strong>
-          <div class="info">banana@banana.co.jp</div>
-          <div class="clear"></div>
-        </div>
-        <div class="copyright">
-          ©2014 Banana Inc. ALL Rights Reserved.
-        </div>
-      </div>
-    </div>
+    <?php get_template_part('layouts/footer') ?>
   </body>
 </html>
