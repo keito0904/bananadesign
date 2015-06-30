93a9fc9478b905c47e3a3eae8a5dc133196ae744
Keito Teranishi <teranishi@undapt.jp>
2015-06-30 21:42:34 +0900

    ヘッダーのテンプレート化

---
+++ layouts/header.php
@@ -0,0 +1,25 @@
+<nav class="top-nav <?php if( is_page( array('about','service','blog')) ) { echo 'shadow'; }else{} ?>">
+  <ul class="content">
+    <li class="logo">
+      <a href="<?php bloginfo('url'); ?>" class="nav-logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png"></a>
+    </li>
+    <li class="list-border"></li>
+    <li>
+      <a href="<?php bloginfo('url'); ?>" class="nav-link <?php if ( is_page('top') ) { echo 'active'; } ?>">ホーム</a>
+    </li>
+    <li class="list-border"></li>
+    <li>
+      <a href="<?php bloginfo('url'); ?>/about" class="nav-link <?php if ( is_page('about') ) { echo 'active'; } ?>">組織概要</a>
+    </li>
+    <li class="list-border"></li>
+    <li>
+      <a href="<?php bloginfo('url'); ?>/service" class="nav-link <?php if ( is_page('service') ) { echo 'active'; } ?>">事業内容</a>
+    </li>
+    <li class="list-border"></li>
+    <li>
+      <a href="<?php bloginfo('url'); ?>/blog" class="nav-link <?php if ( is_page('blog') ) { echo 'active'; } ?>">ブログ</a>
+    </li>
+    <li class="list-border"></li>
+    <div class="clear"></div>
+  </ul>
+</nav>
