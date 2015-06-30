a260fa23120f3205a51ab48f2d809096bbfa2c20
Keito Teranishi <teranishi@undapt.jp>
2015-06-30 15:23:24 +0900

    リンク先の追加

--- top.php
+++ top.php
@@ -20,23 +20,23 @@ Template Name:top
     <nav class="top-nav">
       <ul class="content">
         <li class="logo">
-          <a href="" class="nav-logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png"></a>
+          <a href="<?php bloginfo('url'); ?>" class="nav-logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png"></a>
         </li>
         <li class="list-border"></li>
         <li>
-          <a href="" class="nav-link">ホーム</a>
+          <a href="<?php bloginfo('url'); ?>" class="nav-link">ホーム</a>
         </li>
         <li class="list-border"></li>
         <li>
-          <a href="" class="nav-link">組織概要</a>
+          <a href="<?php bloginfo('url'); ?>/about" class="nav-link">組織概要</a>
         </li>
         <li class="list-border"></li>
         <li>
-          <a href="" class="nav-link">事業内容</a>
+          <a href="<?php bloginfo('url'); ?>/service" class="nav-link">事業内容</a>
         </li>
         <li class="list-border"></li>
         <li>
-          <a href="" class="nav-link">ブログ</a>
+          <a href="<?php bloginfo('url'); ?>/blog" class="nav-link">ブログ</a>
         </li>
         <li class="list-border"></li>
         <div class="clear"></div>
